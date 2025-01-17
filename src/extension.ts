06d8dcac36e048ffdc7e2fb2e9a9862249cdb5a5
cauwt <cauwt@hotmail.com>
2025-01-17 16:38:08 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,73 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zyloh-sql-formatter" is now active!');
+	console.log('"zyloh-sql-formatter" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('zyloh-sql-formatter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from zyloh-sql-formatter!');
+	// Register SQL formatter command
+	let disposable = vscode.commands.registerCommand('zyloh-sql-formatter.format', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			if (document.languageId === 'sql') {
+				// Add your SQL formatting logic here
+				vscode.window.showInformationMessage('SQL formatting triggered!');
+			}
+		}
+	});
+
+	// Register SQL show command
+	let showSqlDisposable = vscode.commands.registerCommand('zyloh-sql-formatter.showCurrentSql', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			if (document.languageId === 'sql') {
+				const position = editor.selection.active;
+				const text = document.getText();
+				
+				// 计算光标在整个文本中的下标
+				const offset = document.offsetAt(position);
+				
+				const currentSql = extractCurrentSql(text, offset);
+				if (currentSql) {
+					// 同时显示SQL和光标位置
+					vscode.window.showInformationMessage(`Position: ${offset}, SQL: ${currentSql}`);
+				}else {
+					vscode.window.showInformationMessage(`No SQL found at position: ${offset}`);
+				}
+				
+			}
+		}
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(showSqlDisposable);
+}
+
+function extractCurrentSql(text: string, cursorPosition: number): string {
+	// 分隔符是分号
+	const delimiters = /;/g;
+	let match;
+	let startPos = 0;
+	let endPos = text.length;
+	
+	// 查找光标所在的SQL语句
+	while ((match = delimiters.exec(text)) !== null) {
+		if (match.index >= cursorPosition) {
+			endPos = match.index;
+			break;
+		}
+		startPos = match.index + 1;
+	}
+	
+	// 提取并清理SQL语句
+	let sql = text.substring(startPos, endPos).trim();
+	
+	// 如果是空的，返回null
+	if (!sql) {
+		return '';
+	}
+	
+	return sql;
 }
 
 // This method is called when your extension is deactivated
