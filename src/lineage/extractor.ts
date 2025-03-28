394a6777362ef609fcf095341a554546adea70f6
cauwt <cauwt@hotmail.com>
2025-03-28 16:37:48 +0800

    update

--- src/lineage/extractor.ts
+++ src/lineage/extractor.ts
@@ -1,25 +1,24 @@
-import { FormatOptions } from '../FormatOptions';
-
 import { createParser } from '../parser/createParser';
 import { StatementNode } from '../parser/ast';
 import { Dialect } from '../dialect';
+import { LineageOptions } from 'src/sqlLineager';
 
 export default class Extractor {
   private dialect: Dialect;
-  private cfg: FormatOptions;
+  private cfg: LineageOptions;
 
-    constructor(dialect: Dialect, cfg: FormatOptions) {
-        this.dialect = dialect;
-        this.cfg = cfg;
-    }
-    
-    public extract(sql: string): StatementNode[] {
-        const ast = this.parse(sql);
-        return ast;
-    }
+  constructor(dialect: Dialect, cfg: LineageOptions) {
+    this.dialect = dialect;
+    this.cfg = cfg;
+  }
 
-    private parse(sql: string): StatementNode[] {
-        return createParser(this.dialect.tokenizer).parse(sql, this.cfg.paramTypes || {});
-      }
-    
+  public extract(sql: string): StatementNode[] {
+    const ast = this.parse(sql);
+    return ast;
+  }
+
+  private parse(sql: string): StatementNode[] {
+    return createParser(this.dialect.tokenizer).parse(sql, {});
+  }
+
 }
